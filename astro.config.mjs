c3edbf723d8a38db05fcc0bc53f412ed546e38c7
Ridzwan <4947140+omgsian@users.noreply.github.com>
2023-07-30 10:10:45 +0800

    updated astra.config.js defineConfig.site

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://omgsian.github.io/", // replace this with your deployed domain
-  base: "/cahaya",
+  site: "https://omgsian.github.io/cahaya", // replace this with your deployed domain
   integrations: [
     tailwind({
       config: {
